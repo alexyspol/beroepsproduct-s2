b5ba76bfe72bcf305c50a9b9e48c55e7bbbd3a1b
alexyspol <alexsandroyspol@gmail.com>
2023-05-04 00:32:00 -0300

    Add DisplayTeamsOption

--- src/main/java/com/dynamis/options/DisplayTeamsOption.java
+++ src/main/java/com/dynamis/options/DisplayTeamsOption.java
@@ -1,12 +1,41 @@
 package com.dynamis.options;
 
+import java.sql.Statement;
+
+import com.dynamis.SqlFileReader;
+
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DisplayTeamsOption implements Option {
 
   @Override
-  public void run(Connection c) {
+  public void run(Connection c) throws SQLException, IOException {
     
+    Statement s = c.createStatement();
+    ResultSet rs = s.executeQuery(SqlFileReader.read("src/main/resources/sqlite/display_teams.sql"));
+
+    String currentTeam = "";
+    int i = 1;
+
+    while(rs.next()) {
+      String teamName = rs.getString("team_name");
+      String fullName = rs.getString("full_name");
+
+      if(!teamName.equals(currentTeam)) {
+        System.out.println();
+        
+        System.out.println("> " + i + ". " + teamName);
+        currentTeam = teamName;
+      }
+
+      System.out.println("    " + fullName);
+      i++;
+    }
+
+    System.out.println();
   }
 
 }
---
+++ src/main/resources/sqlite/display_teams.sql
@@ -0,0 +1,3 @@
+SELECT t.team_name, u.first_name || ' ' || u.last_name AS full_name
+FROM teams t, users u
+WHERE u.team_id = t.id;
