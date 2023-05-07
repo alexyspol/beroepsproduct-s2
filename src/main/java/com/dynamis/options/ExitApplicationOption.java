12a5aa7928b4f390f95b57389143097f177970b4
alexyspol <alexsandroyspol@gmail.com>
2023-05-07 01:18:36 -0300

    Update ExitApplicationOption.java

--- src/main/java/com/dynamis/options/ExitApplicationOption.java
+++ src/main/java/com/dynamis/options/ExitApplicationOption.java
@@ -6,7 +6,7 @@ public class ExitApplicationOption implements Option {
 
     @Override
     public void run(App app) {
-        System.out.println("\n> Goodbye.\n");
+        System.out.println("\n> Goodbye\n");
         app.setUserWantsToExit(true);
     }
 
